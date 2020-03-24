e3eadf7d9d0067b821f3a9abef5f99d279cab05c
Jeferson Oliveira <Jefersonoliver7@gmail.com>
2020-03-23 21:54:39 -0300

    Dicce base project

---
+++ dicee-flutter/ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=/Users/jeferson/Enviroments/flutter-sdk/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/jeferson/Projects/Flutter/flutter-bootcamp/dicee-flutter"
+export "FLUTTER_TARGET=/Users/jeferson/Projects/Flutter/flutter-bootcamp/dicee-flutter/lib/main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "SYMROOT=${SOURCE_ROOT}/../build/ios"
+export "OTHER_LDFLAGS=$(inherited) -framework Flutter"
+export "FLUTTER_FRAMEWORK_DIR=/Users/jeferson/Enviroments/flutter-sdk/flutter/bin/cache/artifacts/engine/ios"
+export "FLUTTER_BUILD_NAME=1.0.0"
+export "FLUTTER_BUILD_NUMBER=1"
+export "TRACK_WIDGET_CREATION=true"
