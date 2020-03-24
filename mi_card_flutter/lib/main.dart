8297d79c94b4e292b206790f1305f783d67cf9e3
Jeferson Oliveira <Jefersonoliver7@gmail.com>
2020-03-23 21:32:21 -0300

    Improving layout with ListTitle and Card

--- mi_card_flutter/lib/main.dart
+++ mi_card_flutter/lib/main.dart
@@ -8,7 +8,7 @@ void main() {
 
 class MyApp extends StatelessWidget {
 
-  var mainColor = Colors.red[600];
+  var mainColor = Colors.red[700];
   var contrastColor = Colors.white;
   @override
   Widget build(BuildContext context) {
@@ -18,65 +18,53 @@ class MyApp extends StatelessWidget {
             body: SafeArea(
               child: Padding(
                 padding: EdgeInsets.all(24),
-                child: Center(
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: <Widget>[
-                      CircleAvatar(
-                          radius: 70,
-                          backgroundImage: NetworkImage("https://media-exp1.licdn.com/dms/image/C4E03AQFxKThmCncP1A/profile-displayphoto-shrink_200_200/0?e=1590624000&v=beta&t=MopALICUOTFtzRw6df9EOh55f7fvEH-UjcoK3DjiFmg"),
-                      ),
-                      Text(
-                        "Jeferson Oliveira",
-                        style: TextStyle(fontWeight: FontWeight.bold, color: contrastColor, fontSize: 40, fontFamily: "Pacifico"),
-                      ),
-                      Text(
-                        "iOS and Flutter Developer",
-                        style: TextStyle(color: contrastColor, fontSize: 16, fontFamily: "SourceSansPro"),
-                      ),
-                      Container(
-                        margin: EdgeInsets.symmetric(vertical: 8),
-                        color: Colors.amber,
-                        width: 150,
-                        height: 1,
-                      ),
-                      Container(
-                        height: 56,
-                        margin: EdgeInsets.symmetric(vertical: 8),
-                        padding: EdgeInsets.fromLTRB(16, 8, 16, 8),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    CircleAvatar(
+                        radius: 70,
+                        backgroundImage: NetworkImage("https://media-exp1.licdn.com/dms/image/C4E03AQFxKThmCncP1A/profile-displayphoto-shrink_200_200/0?e=1590624000&v=beta&t=MopALICUOTFtzRw6df9EOh55f7fvEH-UjcoK3DjiFmg"),
+                    ),
+                    Text(
+                      "Jeferson Oliveira",
+                      style: TextStyle(fontWeight: FontWeight.bold, color: contrastColor, fontSize: 40, fontFamily: "Pacifico"),
+                    ),
+                    Text(
+                      "iOS and Flutter Developer",
+                      style: TextStyle(color: contrastColor, fontSize: 16, fontFamily: "SourceSansPro"),
+                    ),
+                    SizedBox(
+                      width: 100,
+                      child: Divider(
                         color: contrastColor,
-                        child: Row(
-                          children: <Widget>[
-                            Icon(
-                              Icons.phone,
-                              color: mainColor,
-                              size: 24,
-                            ),
-                            Padding(padding: EdgeInsets.all(8)),
-                            Text('+55 71 98337-7911', style: TextStyle(color: mainColor, fontWeight: FontWeight.bold),)
-                          ],
-                        ),
                       ),
-                      Container(
-                        height: 56,
-                        margin: EdgeInsets.symmetric(vertical: 8),
-                        padding: EdgeInsets.fromLTRB(16, 8, 16, 8),
-                        color: contrastColor,
-                        child: Row(
-                          children: <Widget>[
-                            Icon(
-                              Icons.mail,
-                              color: mainColor,
-                              size: 24,
-                            ),
-                            Padding(padding: EdgeInsets.all(8)),
-                            Text('jefersonoliver7@gmail.com', style: TextStyle(color: mainColor, fontWeight: FontWeight.bold),)
-                          ],
+                    ),
+                    Card(
+                      margin: EdgeInsets.symmetric(vertical: 8),
+                      color: contrastColor,
+                      child: ListTile(
+                          leading: Icon(
+                            Icons.phone,
+                            color: mainColor,
+                            size: 24,
+                          ),
+                          title: Text('+55 71 98337-7911', style: TextStyle(color: mainColor, fontWeight: FontWeight.bold),),
+                      ),
+                    ),
+                    Card(
+                      margin: EdgeInsets.symmetric(vertical: 8),
+                      color: contrastColor,
+                      child: ListTile(
+                        leading: Icon(
+                          Icons.mail,
+                          color: mainColor,
+                          size: 24,
                         ),
+                        title: Text('jefersonoliver7@gmail.com', style: TextStyle(color: mainColor, fontWeight: FontWeight.bold),),
                       )
+                    )
 
-                    ],
-                  ),
+                  ],
                 ),
               ),
             ),
